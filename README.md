eb89384c22450816c04229a13a432cf877caaf63
Michael Charles Aubrey <michael@akita-itlabo.com>
2020-07-22 10:25:05 +0900

    implement a different way of obtaining the IP address

--- README.md
+++ README.md
@@ -11,3 +11,7 @@ npm i -g mylip
 ```
 
 After installing, simply run `mylip` from the command line and you'll see your local IPv4 address printed in the terminal.
+
+## Release notes
+
+* 0.2.0 - I was getting inconsistent results with the tool, and so I look into a different way of grabbing the local IP address. I came across this [Stack Overflow](https://stackoverflow.com/a/3742915/5348509) post and mostly copied it verbatim. It should now work more consistently across platforms and return the correct local IP address.
--- index.js
+++ index.js
@@ -1,19 +1,54 @@
 module.exports = () => {
-  const os = require("os");
-  const interfaces = os.networkInterfaces();
+  const getNetworkIPs = (() => {
+    const ignoreRE = /^(127\.0\.0\.1|::1|fe80(:1)?::1(%.*)?)$/i;
 
-  let networkIP = "";
-  Object.keys(interfaces).forEach(function(interfaceName) {
-    interfaces[interfaceName].forEach(function(networkInterface) {
-      if (
-        "IPv4" !== networkInterface.family ||
-        networkInterface.internal !== false
-      ) {
+    const exec = require('child_process').exec;
+    let cached;
+    let command;
+    let filterRE;
+
+    switch (process.platform) {
+      case 'win32':
+        command = 'ipconfig';
+        filterRE = /\bIPv[46][^:\r\n]+:\s*([^\s]+)/g;
+        break;
+      case 'darwin':
+        command = 'ifconfig';
+        filterRE = /\binet\s+([^\s]+)/g;
+        break;
+      default:
+        command = 'ifconfig';
+        filterRE = /\binet\b[^:]+:\s*([^\s]+)/g;
+        break;
+    }
+
+    return (callback, bypassCache) => {
+      if (cached && !bypassCache) {
+        callback(null, cached);
         return;
       }
-      networkIP = networkInterface.address;
-    });
-  });
+      
+      exec(command, (error, stdout, sterr) => {
+        cached = [];
+        let ip;
+        let matches = stdout.match(filterRE) || [];
+        
+        for (let i = 0; i < matches.length; i++) {
+          ip = matches[i].replace(filterRE, '$1')
+          if (!ignoreRE.test(ip)) {
+            cached.push(ip);
+          }
+        }
+        
+        callback(error, cached);
+      });
+    };
+  })();
 
-  console.log(networkIP);
+  getNetworkIPs(function (error, ipAddresses) {
+    ipAddresses.forEach(ip => console.log(ip))
+    if (error) {
+      console.log('error:', error);
+    }
+  }, false);
 };
